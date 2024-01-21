4b328e9dadb916473dd5800a6740ec61c85ab986
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2024-01-21 13:51:32 +0100

    fix: page/pageSize must be interger

--- src/infra/http/common/pagination.schema.ts
+++ src/infra/http/common/pagination.schema.ts
@@ -2,9 +2,10 @@ import { PageSizePolicy } from "@/core/policies/page-size.policy";
 import z from "zod";
 
 export const paginationSchema = z.object({
-  page: z.coerce.number().min(1).optional(),
+  page: z.coerce.number().min(1).int().optional(),
   pageSize: z.coerce
     .number()
+    .int()
     .min(1)
     .max(PageSizePolicy.MAX_PAGE_SIZE)
     .optional(),
